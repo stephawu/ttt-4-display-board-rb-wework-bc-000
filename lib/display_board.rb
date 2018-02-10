c71463acce14dfb1884493030d12c69971519d21
stephanie wu <stephanie.wu@wework.com>
2018-02-10 17:43:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,11 +5,11 @@
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
 def display_board
-  puts "   |   |"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 display_board
